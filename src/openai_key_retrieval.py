ac9784a49a1dae3d404b9c06afbc1ac3df7a45d8
Jan Zimny <janzimny30@gmail.com>
2023-12-01 17:53:30 +0100

    Added a simpler way to set the OpenAI key

--- src/openai_key_retrieval.py
+++ src/openai_key_retrieval.py
@@ -1,11 +1,23 @@
 import json
 import os
+import configparser
+from pyprojroot.here import here
+
 
 def _get_keys(path):
     with open(path) as f:
         return json.load(f)
-    
+
+
 def get_openai_key():
-    full_path_api_key = os.path.expanduser("~/.secret/secret.json")
-    keys = _get_keys(full_path_api_key)
-    return keys["open_ai"]
+    config = configparser.ConfigParser()
+    config.read(here("config.ini"))
+    KEY = config.get("OPENAI", "KEY")
+
+    if KEY == "NA":
+        full_path_api_key = os.path.expanduser("~/.secret/secret.json")
+        keys = _get_keys(full_path_api_key)
+        return keys["open_ai"]
+
+    else:
+        return KEY
