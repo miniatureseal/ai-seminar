df5a8f9d2223b19acdc148275957b4567418305d
Jan Zimny <janzimny30@gmail.com>
2023-12-24 03:00:34 +0100

    Minor last tweak to prompt

--- src/template_generator.py
+++ src/template_generator.py
@@ -14,12 +14,12 @@ class TemplateGenerator:
     _CONTEXT_INFO_JOB_SEEKER = "User {active_user_id} is a job seeker and is writing with a representant from a company which offers a job he is interested in. The context includes past interactions and discussions that occurred between the job seeker {active_user_id} and companies he is interested in. The replies should address all questions and open points of the latest message from the company representative.\n"
 
     _PROMPT_START = (
-        "The replies should only include relevant information known through the context messages from previous chats and is needed to answer the text. Avoid making up new information and don't deviate from the user's writing style. Also, please ensure that all replies convey the same information but are rephrased differently.\n\n"
+        "The replies should only include relevant information known through the context messages from previous chats and is needed to answer the text. Avoid making up new information and don't deviate from the user's writing style. Also, please ensure that all replies convey the same information but are rephrased differently. Don't greet the user which you are replying to again if you already greeted him in the chat history.\n\n"
         "Context:"
         "------------\n"
         "{previous_chat_context}\n"
         "------------\n"
-        "The chat so far:"
+        "Chat history:"
         "------------\n"
         "{chat_history}\n"
         "------------\n"
